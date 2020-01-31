c0a6cfa1881ee9d33f1a72525b8909f0f9365110
Yuanzhe Liu <liuyuanzhe0515@gmail.com>
2020-01-31 12:50:00 +0800

    1.31

--- source/note/index.js
+++ source/note/index.js
@@ -1,21 +1,21 @@
 // 声明 class
-var objid = '5df3844ab7166d000836de12';
+var objid = '5e33b120ff02830008194823';
 function post()
 {
-	var Todo = AV.Object.extend('Todo');
+	var Note = AV.Object.extend('Note');
 	//获取当前时间
 	var date = new Date();
 
 	// 构建新对象
-	var todo = new Todo();	
+	var Note = new Note();	
 	// 为属性赋值
-	todo.set('time', date);
-	todo.set('content', '周二两点，全体成员');
+	Note.set('time', date);
+	Note.set('content', '周二两点，全体成员');
 
 	// 将对象保存到云端
-	todo.save().then(function (todo) {
+	Note.save().then(function (Note) {
 	  // 成功保存之后，执行其他逻辑
-	  console.log('保存成功。objectId：' + todo.id);
+	  console.log('保存成功。objectId：' + Note.id);
 	}, function (error) {
 	  // 异常处理
 	  
@@ -24,54 +24,40 @@ function post()
 
 function get()
 {
-	var query = new AV.Query('Todo');
-	query.get(objid).then(function (todo) {
-		// todo 就是 objectId 为 5df3844ab7166d000836de12 的 Todo 实例
-		var time    = todo.get('time');
-		var content = todo.get('content');
-		console.log('查询成功。time：' + time + ',content:'+content);
+	var query = new AV.Query('Note');
+	query.get(objid).then(function (Note) {
+		// Note 就是 objectId 为 5df3844ab7166d000836de12 的 Note 实例
+		var time    = Note.get('time');
+		var content = Note.get('content');
+		$('#footer').html('查询成功');
 		$("#content").val(content);
 		keyup();
+	},function (error) {
+		$('#footer').html('查询失败');
 	});
 }
 
 function update()
 {
-	var todo = AV.Object.createWithoutData('Todo', objid);
+	var Note = AV.Object.createWithoutData('Note', objid);
 	var content = $("#content").val();
-	todo.set('content', content);
-	todo.save().then(function (todo) {
+	Note.set('content', content);
+	Note.save().then(function (Note) {
 	  // 成功保存之后，执行其他逻辑
-	  console.log('修改成功。objectId：' + todo.id);
+	  //console.log('修改成功。objectId：' + Note.id);
+	  $('#footer').html('修改成功');
 	}, function (error) {
 	  // 异常处理
-	  
+	  $('#footer').html('修改失败');
 	});
 }
-function keyup()
-{
-	var limitNum = 500;
-    var pattern = '还可以输入' + limitNum + '字符';
-	//初始化
-	var remain = $('#content').val().length;
-    if(remain > 500){
-		pattern = "字数超过限制！";
-	}else{
-		var result = limitNum - remain;
-		pattern = '还可以输入' + result + '字符';
-	}
-	$('#contentwordage').html(pattern);
-	console.log(pattern);
-	console.log("keyup");
-}
-//按下某些按键
+
 $(document).ready(function(){
+	get();
     $('#content').keyup(
 		function(){
-			keyup();
 			update();
         }
     );
 });
 
-get();
